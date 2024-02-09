e1c0797f81655153cd48e519de8ba624ffdc7423
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-09 02:53:17 -0800

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5518687442").split()))
-for x in vaporeon:
+for x in ALTRON:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="6084527452"))
 SUDO_USERS.append(OWNER_ID)
