3184584600c27bf589d165d69cbdaa2e988574b5
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-09 02:41:06 -0800

    Update echo.py

--- AltBots/modules/echo.py
+++ AltBots/modules/echo.py
@@ -5,7 +5,7 @@ from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from AltBots.data import VAPOREON
+from AltBots.data import ALTRON
 
 ECHO = []
 
