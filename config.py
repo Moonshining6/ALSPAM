a30fa2418cf3c89c3b20bb7ecfbc34971b067f8b
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-02-09 02:20:24 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from telethon import TelegramClient
 
 from os import getenv
-from AltBots.data import ALTRON
+from AltBots.data import vaporeon
 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
@@ -28,7 +28,7 @@ BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5518687442").split()))
-for x in ALTRON:
+for x in vaporeon:
     SUDO_USERS.append(x)
 OWNER_ID = int(getenv("OWNER_ID", default="5059737154"))
 SUDO_USERS.append(OWNER_ID)
